b044c5eb91f21a396a6ae0e5281d7d438f9b7be4
c1e4 <c1e4.rg@zohomail.com>
2022-09-22 10:32:34 +0400

    Make custom output chunk names for js and css files

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,12 +5,15 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const outputJSFileName = "script";
+const outputCSSFileName = "style";
 
 module.exports = merge(common, {
     mode: "production",
     output: {
-        filename: "[name]-[contenthash].js",
+        filename: `${outputJSFileName}-[contenthash].js`,
         path: path.resolve(__dirname, "dist"),
     },
     optimization: {
@@ -28,7 +31,9 @@ module.exports = merge(common, {
         ],
     },
     plugins: [
-        new MiniCssExtractPlugin({ filename: "[name]-[contenthash].css" }),
+        new MiniCssExtractPlugin({
+            filename: `${outputJSFileName}-[contenthash].css`,
+        }),
         new CleanWebpackPlugin(),
     ],
     module: {
